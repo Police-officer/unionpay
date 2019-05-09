81ca8a77e24ac26a4bbc5d4bec168b386effdd88
huahua <1154684329@qq.com>
2019-05-09 17:17:09 +0800

    银联支付-测试通过提交

---
+++ unionpay/src/main/java/com/example/demo/UnipayApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 花花
+ */
+@SpringBootApplication
+public class UnipayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnipayApplication.class, args);
+	}
+}
---
+++ unionpay/src/test/java/com/example/demo/UnipayApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * author 花花
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnipayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
